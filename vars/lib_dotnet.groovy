265ef24f8738f6a162edc2c813f81971a0070efa
Edoardo Cavallo <iweek16@gmail.com>
2026-02-11 23:46:07 +0100

    Update lib_dotnet.groovy

--- vars/lib_dotnet.groovy
+++ vars/lib_dotnet.groovy
@@ -1,24 +1,12 @@
-def lib_docker
-
-def initialize() {
-    this.lib_docker = library('lib_docker').lib_docker
-}
-
 def dotnetTest(Map props = [:]) {
     def COMMAND = "dotnet test"
     // docker exec labycarweb-dotnet dotnet build file.sln ...
-    return lib_docker.dockerExec(props.nameContainer, "$COMMAND \"${props.pathSolution}\" ${props.params}")
+    return "docker exec ${props.nameContainer} ${COMMAND} "${props.pathSolution}" ${props.params}"
 }
 
 //  q[uiet], m[inimal], n[ormal], d[etailed], and diag[nostic]
 def dotnetBuild(Map props = [:]) {
     def COMMAND = "dotnet build -v ${props.verbosity}"
     // docker exec labycarweb-dotnet dotnet build file.sln ...
-    return lib_docker.dockerExec(props.nameContainer, "$COMMAND \"${props.pathSolution}\" ${props.params}")
-}
-
-def call() {
-    if (!this.lib_docker) {
-        initialize()
-    }
+    return "docker exec ${props.nameContainer} ${COMMAND} "${props.pathSolution}" ${props.params}"
 }
