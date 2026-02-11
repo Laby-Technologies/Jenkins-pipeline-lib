349e84a01630245e18b6473b7d09e37a27ae7475
Edoardo Cavallo <iweek16@gmail.com>
2026-02-11 23:46:42 +0100

    Update lib_dotnet.groovy

--- vars/lib_dotnet.groovy
+++ vars/lib_dotnet.groovy
@@ -1,12 +1,12 @@
 def dotnetTest(Map props = [:]) {
     def COMMAND = "dotnet test"
     // docker exec labycarweb-dotnet dotnet build file.sln ...
-    return "docker exec ${props.nameContainer} ${COMMAND} "${props.pathSolution}" ${props.params}"
+    return "docker exec ${props.nameContainer} ${COMMAND} \"${props.pathSolution}\" ${props.params}"
 }
 
 //  q[uiet], m[inimal], n[ormal], d[etailed], and diag[nostic]
 def dotnetBuild(Map props = [:]) {
     def COMMAND = "dotnet build -v ${props.verbosity}"
     // docker exec labycarweb-dotnet dotnet build file.sln ...
-    return "docker exec ${props.nameContainer} ${COMMAND} "${props.pathSolution}" ${props.params}"
+    return "docker exec ${props.nameContainer} ${COMMAND} \"${props.pathSolution}\" ${props.params}"
 }
