0b6049980b6938703e3f19083b037705b609d4f7
Attilio Iurlaro <34812000+AttilioIurlaro@users.noreply.github.com>
2026-02-13 18:15:57 +0100

    Implement dotnetPublish function for Docker
    Added dotnetPublish function to publish .NET applications.

--- vars/lib_dotnet.groovy
+++ vars/lib_dotnet.groovy
@@ -11,3 +11,23 @@ def dotnetBuild(Map props = [:]) {
     // docker exec labycarweb-dotnet dotnet build file.sln ...
     return "docker exec ${props.nameContainer} ${COMMAND} \"${props.pathSolution}\" ${props.params}"
 }
+
+
+/* 
+    nameContainer   : labycar-dotnet
+    folderImages    : /var/images
+    projectFolder   : ./WebAPI/WebAPI.csproj    
+    imageTag        : 0.1.0    
+    imageName       : name of image    
+ */
+def dotnetPublish(Map props = [:]) {
+    def COMMAND = "dotnet publish"
+    def PARAMS = """ \
+        --os linux --arch x64 /t:PublishContainer \
+        -p ContainerArchiveOutputPath="${props.folderImages}/${props.imageName}.tar.gz" \
+        -p ContainerRepository=${props.imageName} \
+        -p ContainerImageTag=${props.imageTag} \
+        """
+    // docker exec labycarweb-dotnet dotnet build file.sln ...
+    return "docker exec ${props.nameContainer} ${COMMAND} ${props.projectFolder} ${PARAMS}"
+}
